91a428404a78cfe4704cd63cf536040bf8de1fd8
Wahyu Kristianto <w.kristories@gmail.com>
2022-02-26 16:50:55 +0700

    CS Fixer

--- src/Console/Command/ViewMakeCommand.php
+++ src/Console/Command/ViewMakeCommand.php
@@ -5,7 +5,7 @@ namespace Ezhasyafaat\LaravelView\Console\Command;
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Filesystem\Filesystem;
 
-class ViewMakeCommand extends GeneratorCommand 
+class ViewMakeCommand extends GeneratorCommand
 {
     /**
      * The console command name.
@@ -23,60 +23,60 @@ class ViewMakeCommand extends GeneratorCommand
 
     /**
      * The type of class being generated.
-     * 
+     *
      * @var string
      */
     protected $type = 'View';
-    
+
     /**
      * Get the stub file for the generator.
-     * 
+     *
      * @return string
      */
     protected function getStub()
     {
-        return __DIR__.'/stubs/view.stub';
+        return __DIR__ . '/stubs/view.stub';
     }
 
     /**
      * Handle the command.
-     * 
+     *
      * @return void
      */
     public function handle()
     {
-        if($this->option('resource')) {
+        if ($this->option('resource')) {
             /** Create a default resource view */
             $resources = [
                 'index',
                 'create',
                 'edit',
-                'show'
+                'show',
             ];
 
-            foreach($resources as $resource) {
-                $dirr   = resource_path('views' . DIRECTORY_SEPARATOR . $this->argument('name') . DIRECTORY_SEPARATOR . str_replace('.', DIRECTORY_SEPARATOR, $resource).'.blade.php');
+            foreach ($resources as $resource) {
+                $dirr = resource_path('views' . DIRECTORY_SEPARATOR . $this->argument('name') . DIRECTORY_SEPARATOR . str_replace('.', DIRECTORY_SEPARATOR, $resource) . '.blade.php');
 
                 $baseView = $this->files->get($this->getStub());
 
                 $directory = dirname($dirr);
 
-                (new Filesystem)->ensureDirectoryExists($directory);
+                (new Filesystem())->ensureDirectoryExists($directory);
 
-                (new Filesystem)->put($dirr, $baseView);
+                (new Filesystem())->put($dirr, $baseView);
 
-                $this->info('Success Create View '.$this->argument('name').'/'.$resource.'.blade.php');
+                $this->info('Success Create View ' . $this->argument('name') . '/' . $resource . '.blade.php');
             }
         } else {
-            $dirr   = resource_path('views' . DIRECTORY_SEPARATOR . str_replace('.', DIRECTORY_SEPARATOR, $this->argument('name')).'.blade.php');
+            $dirr = resource_path('views' . DIRECTORY_SEPARATOR . str_replace('.', DIRECTORY_SEPARATOR, $this->argument('name')) . '.blade.php');
 
             $baseView = $this->files->get($this->getStub());
-            
+
             $directory = dirname($dirr);
 
-            (new Filesystem)->ensureDirectoryExists($directory);
+            (new Filesystem())->ensureDirectoryExists($directory);
 
-            (new Filesystem)->put($dirr, $baseView);
+            (new Filesystem())->put($dirr, $baseView);
 
             $this->info('Success Create View');
         }
--- tests/TestCreateView.php
+++ tests/TestCreateView.php
@@ -8,8 +8,8 @@ class LaravelViewTest extends TestCase
      * Test make a view file.
      */
 
-     public function test_create_view()
-     {
-         $this->artisan('make:view', ['name' => 'Test/TestCreateView'])->assertExitCode(0);
-     }
+    public function test_create_view()
+    {
+        $this->artisan('make:view', ['name' => 'Test/TestCreateView'])->assertExitCode(0);
+    }
 }
